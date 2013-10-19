fe0fd2a1c4cf00b8f71d2407434f34119a5c4690
Nick P Smith <npsmith1990@gmail.com>
2013-10-19 16:12:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crossfithub::Application.config.secret_key_base = 'f00e876d3cdc1d69d2ba4ce61a54653856f17853c2600fac52982b79c71fcd9575bea74cd7b5d79df1191f7d805639e389a6320a0637aa45c9b6c69998f14cb1'
