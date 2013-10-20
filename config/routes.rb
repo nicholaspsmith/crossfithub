7cbe70fa04495642b457ddfef2eb809162e68dcd
Nick P Smith <npsmith1990@gmail.com>
2013-10-20 10:19:21 -0500

    Add Users controller and user/show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Crossfithub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'users#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
