824c947ca8ff5d8f00618d489d42594debb644d6
Nick P Smith <npsmith1990@gmail.com>
2013-10-20 10:43:18 -0500

    Update User show view and routes to take :id param

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Crossfithub::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'users#show'
+  
+  get 'users/:id' => 'users#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
