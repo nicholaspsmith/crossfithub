1a03052d56817b1a91fa9015b9e943232172fe83
Nick P Smith <npsmith1990@gmail.com>
2013-10-20 14:48:46 -0500

    Update application.html.haml to remove erroneous '2'; update users show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :memberships
   has_many :gyms, through: :memberships
+
+  has_many :wods, through: :gyms
 end
