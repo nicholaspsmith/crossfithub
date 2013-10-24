f6a4ff243beff81ab60fd5305427bd548092b613
Nick P Smith <npsmith1990@gmail.com>
2013-10-24 14:41:55 -0500

    Add name field to sign up form, change user migration to not allow null value for name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   
   # Uncomment before deployment
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :badges
   has_many :memberships
