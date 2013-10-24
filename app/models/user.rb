88f3747392935ad3f069231e78890b29c0f146f4
Justin Blaisdell <jsblaisdell@gmail.com>
2013-10-24 17:26:11 -0500

    make High fives and Shames show correct numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@ class User < ActiveRecord::Base
   has_many :completions
   has_many :votes
   has_many :comments
+
+  def self.vote_count(votes, kind)
+    high_fives = 0
+    shames = 0
+    votes.each do |vote|
+      high_fives += 1 if vote.kind == "High-Five"
+      shames     += 1 if vote.kind == "Shame"
+    end
+    if kind == "High-Five"
+      return high_fives
+    elsif kind == "Shame"
+      return shames
+    else
+      return nil
+    end
+  end
 end
