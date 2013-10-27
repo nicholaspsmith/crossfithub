a36376ecbdfd564bbf789fc610396fe8360e5a89
Nick P Smith <npsmith1990@gmail.com>
2013-10-27 18:00:49 -0500

    Add reverse_relationship to User.rb to allow searching by followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,17 @@ class User < ActiveRecord::Base
   has_many :completions
   has_many :votes
   has_many :comments
+
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :followers, :through => :relationships, :source => :follower
   has_many :followed_users, :through => :relationships, :source => :followed
 
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   validates :name, presence: true, length: { maximum: 50 }
-  validates :password, presence: true
+
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
