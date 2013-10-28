32e6e2bc8a94d893e8793135e1bf833d86c35810
Nick P Smith <npsmith1990@gmail.com>
2013-10-28 15:32:04 -0500

    Add more WODS and completions to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@ pushups = Exercise.create(name:"pushups",reps:30,description:"Puts your hands on
 
 # Create WODS
 fran = Wod.create(name:"Fran",rounds:1,description:"A hard ass workout")
+annie = Wod.create(name:"Annie",rounds:1,description:"A workout named after an orphan")
+barbara = Wod.create(name:"Barbara",rounds:1,description:"I like eggs")
+chelsea = Wod.create(name:"Chelsea",rounds:1,description:"A hard ass workout")
+cindy = Wod.create(name:"Cindy",rounds:0,description:"0 rounds means AMRAP")
+diane = Wod.create(name:"Diane",rounds:1,description:"A hard ass workout")
+
 
 # Add Exercises to WODS
 fran.exercises << pushups
@@ -18,14 +24,14 @@ danes.save
 # Create Users
 nick = User.create(name:"Nick Smith",profile_pic:"https://scontent-a-dfw.xx.fbcdn.net/hphotos-frc3/1395212_10202581883612451_466524952_n.jpg",email:"npsmith1990@gmail.com",password:"a;sldkfjgh")
 justin = User.create(name:"Justin Blaisdell",profile_pic:"https://scontent-a-dfw.xx.fbcdn.net/hphotos-ash3/30315_409670294928_6591159_n.jpg",email:"jsblaisdell@gmail.com",password:"password")
-nicole = User.create(name:"Damnit Pank",profile_pic:"https://scontent-b-dfw.xx.fbcdn.net/hphotos-ash3/1174903_828444650107_128913837_n.jpg",email:"damnit@gmail.com",password:"password")
+nicole = User.create(name:"Nicole Pank",profile_pic:"https://scontent-b-dfw.xx.fbcdn.net/hphotos-ash3/1174903_828444650107_128913837_n.jpg",email:"damnit@gmail.com",password:"password")
 clay = User.create(name:"Clay Stewart",profile_pic:"https://scontent-a-dfw.xx.fbcdn.net/hphotos-ash4/1001196_649788227468_1573908158_n.jpg",email:"clamstew@gmail.com",password:"password")
 ami = User.create(name:"Ami Dave",profile_pic:"https://scontent-b-dfw.xx.fbcdn.net/hphotos-ash4/1381259_214908285345527_675206342_n.jpg",email:"ami@gmail.com",password:"password")
 gamble = User.create(name:"Gamble McAdam",profile_pic:"https://scontent-a-dfw.xx.fbcdn.net/hphotos-prn1/14232_217329480475_1349493_n.jpg",email:"gamble@gmail.com",password:"password")
 taylor = User.create(name:"Taylor Smith",profile_pic:"https://scontent-a-dfw.xx.fbcdn.net/hphotos-ash3/1006265_10200950300253958_799024679_n.jpg",email:"taylor@gmail.com",password:"password")
 
 # Create Memberships
-membership = Membership.create()
+membership = Membership.create
 # Associate Gym and Member through Membership
 membership.user = nick
 membership.gym = danes
@@ -34,13 +40,33 @@ membership.save
 
 # Create Completions
 completion = Completion.create
-# Associate completion with users
 completion.wod = fran # TODO: Add wod_id.
 completion.milliseconds = 352000
 completion.save
+
+completion2 = Completion.create
+completion2.wod = annie
+completion2.milliseconds = 234394
+completion2.save
+
+completion3 = Completion.create
+completion3.wod = barbara
+completion3.milliseconds = 234394
+completion3.save
+
+
 # associate completion with user
 nick.completions << completion
 nick.save
+
+nick.completions << completion2
+nick.save
+
+justin.completions << completion3
+justin.save
+
+
+
 
 # Create Comments
 comment = Comment.new
