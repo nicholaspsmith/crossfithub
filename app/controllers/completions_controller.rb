e11a8f9e13d1422d23eefb03ebf0d13c25b38244
Nick P Smith <npsmith1990@gmail.com>
2013-10-28 16:13:58 -0500

    Add comments for what need to be added to completions_controller & users_controller

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -4,7 +4,8 @@ class CompletionsController < ApplicationController
     new_completion = Completion.new
     new_completion.milliseconds = milliseconds[:milliseconds]
     new_completion.user_id = current_user.id
-    new_completion.wod = @current_wod
+    # need something like this \/ (but better, cause it will actually work)
+    # new_completion.wod = @current_wod
     new_completion.save!
     redirect_to users_path, notice: "Workout added!"
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,15 @@ class UsersController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @completion = Completion.new
-    ## change this later
+    ## will need to set today's wod in here somewhere
     #@current_wod = Wod.find(3)
   end
 
   def show
     @user = User.find(params[:id])
     @users = User.all
+    ## will need to set today's wod in here somewhere
+    #@current_wod = Wod.find(3)
   end
 
 end
