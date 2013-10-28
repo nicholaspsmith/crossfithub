c991606869dd19c8006b7500d07530a31ea79a34
Justin Blaisdell <jsblaisdell@gmail.com>
2013-10-28 15:25:23 -0500

    add completion controller, add ability to log completions

---
+++ app/controllers/completions_controller.rb
@@ -0,0 +1,10 @@
+class CompletionsController < ApplicationController
+  def create
+    milliseconds = params[:completion]
+    new_completion = Completion.new
+    new_completion.milliseconds = milliseconds[:milliseconds]
+    new_completion.user_id = current_user.id
+    new_completion.save!
+    redirect_to users_path, notice: "Workout added!"
+  end
+end
