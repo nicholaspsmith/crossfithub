b26b1218f2916baef63eb188d5453a855861f87e
Nick P Smith <npsmith1990@gmail.com>
2013-10-29 20:18:58 -0500

    Search by exact name working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.find(current_user.id)
     @completion = Completion.new
     ## will need to set today's wod in here somewhere
     #@current_wod = Wod.find(3)
     # @workouts = Wod.all.by_name
+    if params[:search]
+      #binding.pry
+      @user = User.find(:all, :conditions => ['name LIKE ?', "%#{params[:search]}%"]).first
+      render "show"
+    else
+      @user = User.find(current_user.id)
+    end
   end
 
   def show
